d1fcf889205c134e0b4153e1f696941c51a5fdea
Ian Littman <iansltx@gmail.com>
2022-10-26 21:17:18 -0500

    Make attendee type dirs automatically, increase screenshot resolution, remove background

--- screenshot.js
+++ screenshot.js
@@ -1,8 +1,15 @@
 const puppeteer = require('puppeteer');           // include lib
+const fs = require('fs');
+
 (async () => {                                    // declare function
   const browser = await puppeteer.launch();       // run browser
   const page = await browser.newPage();           // open new tab
   await page.goto('http://localhost:8000/template.php?screenshot=1');
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 3.5
+  });
 
   await page.waitForSelector('#end-of-page');
 
@@ -15,7 +22,10 @@ const puppeteer = require('puppeteer');           // include lib
 
   for (const item of items) {
     const el = await page.$('#' + item.id);
-    await el.screenshot({ path: item.type + '/' + item.id + '.png' });
+    if (!fs.existsSync(item.type)) {
+      fs.mkdirSync(item.type);
+    }
+    await el.screenshot({ path: item.type + '/' + item.id + '.png', omitBackground: true });
   }
 
   await browser.close();                          // close browser
